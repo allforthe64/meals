c495407a396d4b78833757ef2bf84eec850bf5b9
William Rainey <williamhrainey@gmail.com>
2022-09-02 10:46:50 -0700

    added new meals ejs file

--- controllers/meals.js
+++ controllers/meals.js
@@ -4,16 +4,16 @@ module.exports = {
     getMeals: async (req,res)=>{
         console.log(req.user)
         try{
-            const mealItems = await Meal.find({userId:req.user.id})
+            const mealsItems = await Meal.find({userId:req.user.id})
             const itemsLeft = await Meal.countDocuments({userId:req.user.id,completed: false})
-            res.render('meals.ejs', {meals: mealItems, left: itemsLeft, user: req.user})
+            res.render('meals.ejs', {meals: mealsItems, left: itemsLeft, user: req.user})
         }catch(err){
             console.log(err)
         }
     },
     createMeal: async (req, res)=>{
         try{
-            await Meal.create({meal: req.body.mealItem, completed: false, userId: req.user.id})
+            await Meal.create({meal: req.body.mealsItem, completed: false, userId: req.user.id})
             console.log('Meal has been added!')
             res.redirect('/meals')
         }catch(err){
