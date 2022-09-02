e97e9d032e5bb5319b39fa3f061212ce048e0f39
linds0202 <lindsayarainey@gmail.com>
2022-09-02 10:26:53 -0700

    created meals model

---
+++ models/Meal.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const MealSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Meal', MealSchema)
